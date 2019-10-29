4dafa74eea68d7349f0a286677501067e7c866c1
Chris Johnson <cljohnson4343@gmail.com>
2019-10-28 19:26:51 -0500

    Add room scaffolding.

---
+++ app/src/main/java/com/example/nycschools/data/SchoolDao.kt
@@ -0,0 +1,15 @@
+package com.example.nycschools.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface SchoolDao {
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    fun insertSchools(vararg schools: School)
+
+    @Query("SELECT * FROM ${School.TABLE_NAME}")
+    fun loadSchools(): List<School>
+}
